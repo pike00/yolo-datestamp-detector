757ef82b15c236c99da4ec136a67138f8b3b0ebb
Will Pike <willpike00@gmail.com>
2026-04-15 19:16:15 -0500

    chore: gitignore generated data and debug artifacts

--- dedup/pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-from pipeline.ingest import ingest
-from pipeline.enrich import enrich
-from pipeline.deduplicate import deduplicate
-from pipeline.export import export_canonicals
-
-__all__ = ["ingest", "enrich", "deduplicate", "export_canonicals"]
